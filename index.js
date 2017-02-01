37b89678996a9ef85dfa3e3a4dd0f55bce79a13c
Brian Conery <jack.conery@gmail.com>
2017-02-01 18:44:48 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function add(A, B) {
+return A + B
+}
+function subtract(A,B) {
+  return A - B
+}
+function multiply(A,B) {
+  return A * B
+}
+function divide(A,B) {
+  return A / B
+}
+
+function inc(n) {
+  return ++n
+}
+
+function dec(n) {
+  return --n
+}
+
+function makeInt(n) {
+  return parseInt(n, 10)
+}
+function preserveDecimal(n) {
+  return parseFloat(n)
+}
